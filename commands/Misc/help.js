790325bb4c8c5d22c253f655c7fe290e830ad993
Corentin <48736362+Eowalim@users.noreply.github.com>
2020-07-05 03:03:33 +0200

    Update help.js

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -22,12 +22,12 @@ module.exports.run = (client, message, args) => {
     .addFields(
       {
         name: ":no_entry: Commandes pour le staff",
-        value: `\`${PREFIX}raidmode\`: Permet d'activer ou désactiver le mode raid (empêche l'arrivée de nouveau membre).\n\`${PREFIX}poll question !rep1 !rep2 ...(max 10)\`: Permet de créer un sondage.\n\`${PREFIX}stats\`: Donne les statistiques du jour.`,
+        value: `\`${PREFIX}raidmode\`: Permet d'activer ou désactiver le mode raid (empêche l'arrivée de nouveau membre).\n\`${PREFIX}poll question !rep1 !rep2 ...(max 10)\`: Permet de créer un sondage.`,
         inline: false,
       },
       {
         name: ":page_facing_up: Commandes d'informations",
-        value: `\`${PREFIX}help\`: Donne la fiche du BOT.\n\`${PREFIX}infos\`: Donne les informations du discord.\n\`${PREFIX}user @utilisateur\`: Donne les informations d'un utilisateur.\n\`${PREFIX}nn @utilisateur\`: Donne la liste des pseudos par lequel l'utilisateur est passé.`,
+        value: `\`${PREFIX}help\`: Donne la fiche du BOT.\n\`${PREFIX}infos\`: Donne les informations du discord.\n\`${PREFIX}user @utilisateur\`: Donne les informations d'un utilisateur.`,
         inline: false,
       },
       {
@@ -37,11 +37,11 @@ module.exports.run = (client, message, args) => {
       },
       {
         name: ":information_source:  Informations",
-        value: `1) Pour les pseudos, le BOT récupère que ceux que lui il a détecté depuis son lancement. Les pseudos que vous aviez avant que le bot soit là il ne pourra pas les avoir.\n2) Pour les couleurs vous n'avez plus besoin de retirer vous même l'ancienne pour que la nouvelle s'applique.\n3) Si vous êtes curieux, vous pouvez retorouver le code du bot [ici](https://github.com/Eowalim/Unknown) :wink:.`,
+        value: `1) Pour les couleurs vous n'avez plus besoin de retirer vous même l'ancienne pour que la nouvelle s'applique.\n3) Si vous êtes curieux, vous pouvez retorouver le code du bot [ici](https://github.com/Eowalim/Unknown) :wink:.`,
         inline: false,
       }
     )
-    .setFooter(`Bot by Eowalim v1.0 | En cas de bugs contactez Eowalim`);
+    .setFooter(`Bot by Eowalim v2.0 | En cas de bugs contactez Eowalim`);
 
   message.channel.send(embed);
   message.delete();
